ad8edbe3ed3b17aaaba658ceaea25d77850778eb
OroszBarbara <Orosz.Cs.Barbara@student.utcluj.ro>
2025-01-02 17:45:40 +0200

    added the baords , lists , cards

--- apps/boards/admin.py
+++ apps/boards/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Board, List, Card
 
-# Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'owner', 'created_at')
+    search_fields = ('name', 'description')
+
+@admin.register(List)
+class ListAdmin(admin.ModelAdmin):
+    list_display = ('name', 'board',  'created_at')
+    search_fields = ('name', 'board')
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('name','description', 'list' ,'due_date', 'created_at')
+    search_fields = ('name', 'description', 'list' )
+
+
+
+
--- apps/boards/views.py
+++ apps/boards/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Board, List, Card
+from .serializers import BoardSerializer, ListSerializer, CardSerializer
 
-# Create your views here.
+
+class BoardListCreateView(generics.ListCreateAPIView):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class BoardDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ListCreateView(generics.ListCreateAPIView):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        board = Board.objects.get(id=self.request.data.get('board'))
+        serializer.save(board=board)
+
+
+class CardCreateView(generics.ListCreateAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        list_instance = List.objects.get(id=self.request.data.get('list'))
+        serializer.save(list=list_instance)
